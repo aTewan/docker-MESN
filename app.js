44fce3b44536aa29d93a56cf7d26f2e413a4cce6
aTewan <tewan33@gmail.com>
2020-03-12 15:45:04 +0100

    :card_file_box: Connexion à MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 class App {
     constructor() {
         this.app = express();
+        this.connectToMongo();
+    }
+
+    connectToMongo() {
+        mongoose.Promise = global.Promise;
+        let mongoUri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/`;
+
+        console.log(mongoUri)
+
+        mongoose.connect(mongoUri);
+
+        mongoose.connection.on('open', () => {
+            console.log("Connecté à MongoDB !");
+        });
+
+        mongoose.connection.on('error', () => {
+            console.log("Erreur lors de la connexion à MongoDB !");
+        })
     }
 }
 
