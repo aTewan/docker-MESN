f8a8c2cd912983e49de76cd745c23bb0b7e37d37
aTewan <tewan33@gmail.com>
2020-03-15 21:03:48 +0100

    :sparkles: Ajout des routes pour check un todo et le supprimer

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,11 +1,16 @@
 import * as todoController from '../controllers/todo'
 
 const API_TODOS = "/todos";
+const API_TODO_ID = "/todos/:id";
 
 export class Routes {
     routes(app) {
         app.route(API_TODOS)
                 .get(todoController.getTodos)
                 .post(todoController.addTodo);
+        
+        app.route(API_TODO_ID)
+                .patch(todoController.checkTodo)
+                .delete(todoController.deleteTodo);
     }
 }
