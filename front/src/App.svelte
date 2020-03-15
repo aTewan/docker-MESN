30746a63e3d6fd350d849b579112a87c129fd1ae
aTewan <tewan33@gmail.com>
2020-03-15 17:59:34 +0100

    :sparkles: Ajout du front

---
+++ front/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte app</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		background-color: orangered;
+	}
+
+	h1 {
+		color: whitesmoke;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
